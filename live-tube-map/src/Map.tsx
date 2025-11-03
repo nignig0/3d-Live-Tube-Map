c3aeb017ede1fbb962da0140422aa19231e55759
nignig0 <tanitoluwaadebayo@gmail.com>
2025-11-03 13:53:51 +0000

    1 step closer to getting the trains on the map

--- live-tube-map/src/Map.tsx
+++ live-tube-map/src/Map.tsx
@@ -1,9 +1,8 @@
 import mapboxgl from "mapbox-gl";
 import { useEffect, useRef } from "react";
 import "mapbox-gl/dist/mapbox-gl.css";
-
-
-console.log("MAP TOKEN:", import.meta.env.VITE_MAP_KEY);
+import * as turf from '@turf/turf';
+import { lineString } from '@turf/helpers'
 
 mapboxgl.accessToken = import.meta.env.VITE_MAP_KEY as string;
 const TFL_BASE_URL = "https://api.tfl.gov.uk"
@@ -22,11 +21,11 @@ const lineIdColourMap: { [key: string]: string } = {
     "waterloo-city": "#95CDBA"
 };
 
-interface tubeLineData {
-    id: string, 
-    color: string,
-    coordinates: number[][][]
-};
+const stationNameCoordinateMap: {[key: string]: number[] } = {};
+const lineIdTurfLineStringMap: {[key: string]: any} = {};
+const lineIdStationNamesMap: {[key: string]: Set<string>} = {};
+
+
 
 const getTubeLines = async()=>{
     const request = await fetch(`${TFL_BASE_URL}/Line/Mode/tube?app_key=${import.meta.env.VITE_TFL_API_KEY as string}`);
@@ -41,35 +40,46 @@ const getLineGeometry = async(id: string)=>{
     const response = await request.json();
     return response;
 }
+
+const parseCurrentLocation = (currentLocation: string, stationName?: string)=>{
+    let match;
+    if(match = currentLocation.match(/Between (.+?) and (.+)/)){
+        return {
+            type: 'between',
+            stationA: match[1],
+            stationB: match[2]
+        }
+    }
+
+    if(currentLocation === "At Platform"){
+        return { type: 'atPlatform', station: stationName };
+    }
+
+    if(match = currentLocation.match(/^At (.+)/)){
+        return { type: 'at', station: match[1] };
+    }
+
+    if(match = currentLocation.match(/Approaching (.+)/)){
+        return { type: 'approaching', station: match[1] };
+    }
+
+    if(match = currentLocation.match(/Leaving (.+)/)){
+        return { type: 'leaving', station: match[1] };
+    }
+
+    if (match = currentLocation.match(/Left (.+)/)) {
+        return { type: 'left', station: match[1] };
+    }
+  
+    // Unknown format - log it for debugging
+    console.warn('Unknown currentLocation format:', currentLocation);
+    return null;
+}
+
 export default function Map(){
     const mapContainerRef = useRef<HTMLDivElement | null>(null);
 
     useEffect(()=>{
-        const tubeLineDataArray: tubeLineData[] = [];
-        //we are assuming everything is fine here 
-        //not much error handling
-        getTubeLines().then((data)=>{
-            for(const tubeData of data){
-                const data: tubeLineData  = {
-                    id: tubeData.id,
-                    color: lineIdColourMap[tubeData.id], 
-                    coordinates: []
-                };
-
-                getLineGeometry(tubeData.id).then((tubeLineData)=>{
-                    console.log(tubeLineData.lineStrings);
-                    for(const lineStrings of tubeLineData.lineStrings){
-                        const lineData : number[][][] = JSON.parse(lineStrings);
-                        data.coordinates = lineData;
-                        console.log("parsed line data -> ", lineData);
-                    }
-                    
-                });
-                tubeLineDataArray.push(data);
-                console.log(data);
-                console.log(`Processed ${tubeData.id}`);
-            }
-        });
         if(!mapContainerRef.current) return;
 
         const map = new mapboxgl.Map({
@@ -84,7 +94,32 @@ export default function Map(){
 
         // add navigation controls
         map.addControl(new mapboxgl.NavigationControl(), "top-right");
-        map.on("load", ()=>{
+        map.on("load", async ()=>{
+            //we are getting the data in load first so we can 
+            //have all data before we start building the map
+            const stationData: any[] = [];
+            const tubeLines = await getTubeLines(); //get all tube lines
+            const promises = tubeLines.map(async (tube: any) =>{
+                const geometry = await getLineGeometry(tube.id);
+                const coordinates = geometry.lineStrings.map((ls: string) => JSON.parse(ls));
+                //lineStrings is a list of a list of 2d arrays... we change every element in that
+                //list into its parsed version cause they are strings initially
+                stationData.push(geometry.stations);
+
+                return {
+                    id: tube.id,
+                    color: lineIdColourMap[tube.id],
+                    coordinates: coordinates
+                }
+            });
+
+            const results = await Promise.all(promises);
+            stationData.flat().forEach((station: any)=>{
+                stationNameCoordinateMap[station.name] = [station.lon, station.lat];
+            });
+
+            console.log("Station Name - Coordinate Map -> ", stationNameCoordinateMap);
+
             map.addLayer({
                 id: "3d-buildings",
                 source: "composite",
@@ -100,21 +135,76 @@ export default function Map(){
                 }  
             });
 
-            for(const line of tubeLineDataArray){
+            //we are adding the lines and stops
+            for(const line of results){
+                //flat turns [[a,b], [c, d]] => [a,b,c,d]
+                const stationsFeatures = line.coordinates.flat().flatMap((coordinateList: any)=>{
+                    return coordinateList.map((coordinate: any)=>{
+
+                        return {
+                            type: 'Feature',
+                            geometry: { type: 'Point', coordinates: coordinate },
+                            properties: { line: line.id }
+                        };
+                    });
+                    
+                });
+
+
+                map.addSource(`${line.id}-stations-source`, {
+                    type: 'geojson',
+                    data: {
+                        type: "FeatureCollection",
+                        features: stationsFeatures
+                    }
+                });
+
+                map.addLayer({
+                    id: `${line.id}-stations-layer`,
+                    type: 'circle',
+                    source: `${line.id}-stations-source`,
+                    paint: {
+                        'circle-radius':10,
+                        'circle-color': line.color,
+                        'circle-stroke-width': 10,
+                        'circle-stroke-color': '#fff',
+                    }
+                }, '3d-buildings');
+
             for(let i = 0; i<line.coordinates.length; ++i){
                 console.log("printing from tube line data array -> ", line.coordinates[i]);
-                map.addSource(`${line.id}-line-${i}`, {
+                //so the coordinates are an array of an array of [lat, long] pairs
+                //buuut we have only one array of [lat, long] pairs in our array of arrays
+                const id = `${line.id}-line-${i}`
+                console.log(`Adding Source -> ${id}`);
+                map.addSource(id, {
                 type: "geojson",
                 data: {
                     type: "Feature",
                     geometry: {
                         type: "LineString",
-                        coordinates: line.coordinates[i]
+                        coordinates: line.coordinates[i].flat()
                     },
                     properties: {}
                 }
                 });
+                const turfLineString = turf.lineString(line.coordinates[i].flat());
+                lineIdTurfLineStringMap[id] = turfLineString;
+                //we want to get line id to the various stops as well 
+                //So we can loop through the coordinates and the stationNameCoordinatesMap get the names of the stops 
+                //inefficient I know
+                
+                lineIdStationNamesMap[id] = new Set<string>();
+                for(const [long, lat] of line.coordinates[i].flat()){
+                    for(const stationName of Object.keys(stationNameCoordinateMap)){
+                        const stationLong = stationNameCoordinateMap[stationName][0];
+                        const stationLat = stationNameCoordinateMap[stationName][1];
 
+                        if(long === stationLong && lat === stationLat) lineIdStationNamesMap[id].add(stationName);
+                    }
+                }
+
+                console.log(`Adding Line Layer -> ${line.id}-line-${i}`);
                 map.addLayer({
                     id: `${line.id}-line-layer-${i}`,
                     type: "line",
@@ -125,10 +215,20 @@ export default function Map(){
                     },
                     paint: {
                         "line-color": line.color,
-                        "line-width": 4
+                        "line-width": [
+                            'interpolate', //smooth transitions between line widths when we zoom,
+                            ['linear'], //interpolation type
+                            ['zoom'], //the input variable... current zoom level
+                            10, 10, //at zoom 10, let the width be 3
+                            14, 20, //at zoom 14 let the width be 8
+                        ],
+                        'line-opacity': 0.25
                     }
                 });
+                
             }
+
+            console.log('LineIdStationNamesMap -> ', lineIdStationNamesMap);
         }
         });
 
